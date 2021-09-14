5ac9eb29482ed45490a48478c6c991a61ad33c51
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-09-14 06:35:36 +0200

    aggiunti commenti al main.c

--- app/src/main.c
+++ app/src/main.c
@@ -7,37 +7,56 @@
 #include "path_generation.h"
 
 
-#define POINTS 2
+#define POINTS 2        //Numero di punti inseriti
 
 
 int main(void)
 {
 
-/*
+    /**
+     * I punti vanno insteriti nel vettore 'points_elem', come negli esempi qui sotto.
+     * Devono essere inseriti nella forma di 'vettore colonna', quindi il vettore
+     * sarà riempito prima con le 'x' di tutti i punti, successivamente le 'y' e le 'z'.
+     * Inserire anche il numero di punti nella macro 'POINTS'.
+     */
+
+
+
+
+    /*
     double points_elem[3*POINTS] = {    +0.0,   +0.0,   +0.1,   -0.2,   -0.2,
                                         +0.0,   +0.0,   +0.2,   +0.2,   +0.0,
                                         -0.6,   -0.7,   -0.7,   -0.7,   -0.8};
-*/
+    */
     
     double points_elem[3*POINTS] = {    +0.0,   +0.1,
                                         +0.0,   +0.3,
                                         -0.6,   -0.7};
 
-/*
+    /*
     double points_elem[3*POINTS] = {    +0.4,   -0.1,
                                         +0.0,   +0.3,
                                         -0.6,   -0.8};
-*/
-/*
+    */
+    /*
     double points_elem[3*POINTS] = {    +0.0,   +0.0,    +0.2,   -0.1,   +0.3,   -0.3,   -0.1,
                                         +0.0,   -0.2,    +0.3,   +0.1,   +0.2,   +0.3,   +0.0,
                                         -0.6,   -0.7,    -0.6,   -0.7,   -0.7,   -0.7,   -0.8};
-*/
+    */
 
     matrix *points = fill_matrix(3, POINTS, points_elem);
     matrix *pos = NULL;
     matrix *vel = NULL;
     matrix *acc = NULL;
+
+
+    /**
+     * L'algoritmo può essere impostato dal secondo, terzo e quarto parametro.
+     * Rispettivamente:
+     * - tempo in cui completare il persorso [s]
+     * - precisione con cui l'algoritmo ricerca il percorso ottimale (consigliato < 0.1)
+     * - risoluzione della curva campionata (per ogni tratto tra due punti)
+     */
 
     path(points, 1.0, 0.01, 500, &pos, &vel, &acc);
 
